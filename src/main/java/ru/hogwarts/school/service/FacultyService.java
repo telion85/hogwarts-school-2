b6f22e997b270d4d43a957521b3d3e6ea5dbab00
Telion <www.neon@mail.ru>
2023-02-09 17:10:25 +0500

    добавил методы для фильтрации в сервисы

--- src/main/java/ru/hogwarts/school/service/FacultyService.java
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -4,26 +4,37 @@ import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class FacultyService {
     private final Map<Long, Faculty> facultyMap = new HashMap<>();
     private static Long counterId;
 
-    public Faculty create(Faculty faculty) {
-        return facultyMap.put(counterId++, faculty);
+    public Faculty create(String name, String color) {
+        return facultyMap.put(counterId, new Faculty(counterId++, name, color));
     }
 
     public Faculty read(Long facultyId) {
         return facultyMap.get(facultyId);
     }
 
-    public Faculty update(Long facultyId, Faculty faculty) {
+    public Faculty update(Long facultyId, String name, String color) {
+        Faculty faculty = facultyMap.get(facultyId);
+        faculty.setName(name);
+        faculty.setColor(color);
         return facultyMap.put(facultyId, faculty);
     }
 
     public Faculty delete(Long facultyId) {
         return facultyMap.remove(facultyId);
+    }
+
+    public List<Faculty> filterByColor(String color) {
+        return facultyMap.values().stream()
+                .filter(faculty -> faculty.getColor().equals(color))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,37 +1,40 @@
 package ru.hogwarts.school.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Student;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentService {
-    private final Map<Long, Student> repositoryMap = new HashMap<>();
+    private final Map<Long, Student> studentMap = new HashMap<>();
     private static Long counterId = 1L;
-    private final Logger logger = LoggerFactory.getLogger(StudentService.class);
 
-    public Student create(Student student) {
-        return repositoryMap.put(counterId++, student);
+    public Student create(String name, Integer age) {
+        return studentMap.put(counterId, new Student(counterId++, name, age));
     }
 
     public Student read(Long studentId) {
-        return repositoryMap.get(studentId);
+        return studentMap.get(studentId);
     }
 
-    public Student update(Long studentId, Student student) {
-        return repositoryMap.put(studentId, student);
+    public Student update(Long studentId, String name, Integer age) {
+        Student student = studentMap.get(studentId);
+        student.setName(name);
+        student.setAge(age);
+        return student;
     }
 
     public Student delete(Long studentId) {
-        return repositoryMap.remove(studentId);
+        return studentMap.remove(studentId);
     }
 
-
-
-
-
+    public List<Student> filterByAge(Integer age) {
+        return studentMap.values().stream()
+                .filter(student -> student.getAge() == age)
+                .collect(Collectors.toList());
+    }
 }
