ba6c76d572e3c8d50cf3c0ce8c0d0ae435ceed3e
Telion <www.neon@mail.ru>
2023-02-09 14:14:39 +0500

    создал модель и две сущности Student и Faculty

---
+++ src/main/java/ru/hogwarts/school/model/Faculty.java
@@ -0,0 +1,62 @@
+package ru.hogwarts.school.model;
+
+public class Faculty {
+    private final Long id;
+    private String name;
+    private String color;
+
+    public Faculty(Long id, String name, String color) {
+        this.id = id;
+        this.name = name;
+        this.color = color;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Faculty faculty = (Faculty) o;
+
+        if (id != null ? !id.equals(faculty.id) : faculty.id != null) return false;
+        if (name != null ? !name.equals(faculty.name) : faculty.name != null) return false;
+        return color != null ? color.equals(faculty.color) : faculty.color == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (color != null ? color.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Faculty{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -0,0 +1,55 @@
+package ru.hogwarts.school.model;
+
+public class Student {
+    private final Long id;
+    private String name;
+    private int age;
+
+    public Student(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Student student = (Student) o;
+
+        if (age != student.age) return false;
+        if (id != null ? !id.equals(student.id) : student.id != null) return false;
+        return name != null ? name.equals(student.name) : student.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + age;
+        return result;
+    }
+
+
+}
